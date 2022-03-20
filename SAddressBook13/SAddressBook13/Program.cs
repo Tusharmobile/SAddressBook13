d9c31fd025dfcd184915bc85f969cdead9a3a3f8
Tushar <tusharshitre123@gmail.com>
2022-03-20 22:02:20 +0530

    add read write file io

---
+++ SAddressBook13/SAddressBook13/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            SAddressBook newaddressbook = new SAddressBook();
+            Contact newcontact = new Contact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Number to Execute the Address book Program \n1. Create contacts \n2. Add contact \n3. Edit contact \n4. Delete contact \n5. Add contact \n6. Add multiple Address Book with unique name \n7. Check For Duplicate \n8. Search person by city or state \n9. View person by city or state \n10.Count person by city or state \n11. Sort entries using person name \n12. Read  write IO file \n13. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        newaddressbook.AddContact(newcontact);
+                        newaddressbook.Display();
+                        break;
+                    case 3:
+                        newaddressbook.Editexistingcontact();
+                        newaddressbook.Display();
+                        break;
+                    case 4:
+                        newaddressbook.Deletecontact();
+                        newaddressbook.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        newaddressbook.AddContact(newcontact);
+                        newaddressbook.Display();
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter the Unique Contact to be added: ");
+                        string name = Console.ReadLine();
+                        newaddressbook.AddUniqueContact(name);
+                        newaddressbook.DisplayUniqueContacts();
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter the Unique Contact to be added: ");
+                        string duplicatename = Console.ReadLine();
+                        newaddressbook.AddUniqueContact(duplicatename);
+                        newaddressbook.DisplayUniqueContacts();
+                        break;
+                    case 8:
+                        newaddressbook.Search_person_city_state();
+                        break;
+                    case 9:
+                        newaddressbook.view_person_city_state();
+                        break;
+                    case 10:
+                        newaddressbook.Count_person_city_state();
+                        break;
+                    case 11:
+                        newaddressbook.SortbyName();
+                        break;
+                    case 12:
+                        newaddressbook.Readfile();
+                        newaddressbook.WriteUsingStreamWriter();
+                        break;
+                    case 13:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SAddressBook13/Unit Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Unit Testing")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0cd75bc-319e-4022-8f49-8f4cc984b38d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
